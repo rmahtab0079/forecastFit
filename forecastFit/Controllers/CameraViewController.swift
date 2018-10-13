cf7f9167f7aea6da016ecf8acbd8b762f1f38902
Rageeb Mahtab <>
2018-10-13 13:52:27 -0400

    Implemented Camera

---
+++ forecastFit/Controllers/CameraViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CameraViewController.swift
+//  forecastFit
+//
+//  Created by Rageeb Mahtab on 10/13/18.
+//  Copyright © 2018 Rageeb Mahtab. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//    @IBOutlet weak var imageToCamera: UIImageView!
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        let didTapImage = UITapGestureRecognizer(target: self, action: #selector(setImagePicker))
+//        imageToCamera.isUserInteractionEnabled = true
+//        imageToCamera.addGestureRecognizer(didTapImage)
+//        
+//        
+//        
+//        // Do any additional setup after loading the view.
+//    }
+//    
+//    @objc func setImagePicker() {
+//        let vc = UIImagePickerController()
+//        vc.delegate = self
+//        vc.allowsEditing = true
+//        
+//        
+//        if UIImagePickerController.isSourceTypeAvailable(.camera)
+//        {
+//            print("camera")
+//            vc.sourceType = .camera
+//        } else {
+//            vc.sourceType = .photoLibrary
+//        }
+//    }
+//    
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        <#code#>
+//    }
+//    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- forecastFit/Controllers/HomeViewController.swift
+++ forecastFit/Controllers/HomeViewController.swift
@@ -10,44 +10,55 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    @IBOutlet weak var highTemperatureLabel: UILabel!
-    
-    @IBOutlet weak var lowTemperatureLabel: UILabel!
-    
+    @IBOutlet weak var highLabel: UILabel!
 
-    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var lowLabel: UILabel!
+    
+    @IBOutlet weak var weatherLabel: UILabel!
     
    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ny%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-            if let error = error{
-                print(error.localizedDescription)
-            } else if let data = data{
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-
-//                print(dataDictionary)
-
-                let query = dataDictionary["query"] as! [String: Any]
-                let results = query["results"] as! [String: Any]
-                let channel = results["channel"] as! [String: Any]
-                let item = channel["item"] as! [String: Any]
-                let forecast = item["forecast"] as! [[String: Any]]
-                let today = forecast[0]
-                let highTemp = today["high"] as! String
-                let lowTemp = today["high"] as! String
-                let weatherDescription = today["text"] as! String
-                self.highTemperatureLabel.text = highTemp
-                self.lowTemperatureLabel.text = lowTemp
-                self.weatherDescriptionLabel.text = weatherDescription
-                
-            }
-        }
-        task.resume()
+        
+//        let url = URL(string: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ny%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")!
+//        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+//        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+//        let task = session.dataTask(with: request) { (data, response, error) in
+//            if let error = error{
+//                print(error.localizedDescription)
+//            } else if let data = data{
+//                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+//
+//        
+//
+//                let query = dataDictionary["query"] as! [String: Any]
+//              
+//                let results = query["results"] as! [String: Any]
+//    
+//                let channel = results["channel"] as! [String: Any]
+//    
+//                let item = channel["item"] as! [String: Any]
+//
+//                let forecast = item["forecast"] as! [[String: Any]]
+//                
+//                print(forecast)
+//
+//                let today = forecast[0]
+//     
+//                let highTemp = today["high"] as! String
+// 
+//                let lowTemp = today["low"] as! String
+//
+//                let weatherDescription = today["text"] as! String
+//       
+//                self.highLabel.text = highTemp
+//                self.lowLabel.text = lowTemp
+//                self.weatherLabel.text = weatherDescription
+//                
+//            }
+//        }
+//        task.resume()
         
         // Do any additional setup after loading the view.
 
