d5ede6044cb44697d3636c69879d9b1873a1c015
Rageeb Mahtab <rmahtab0079@bths.edu>
2018-10-13 02:14:13 -0400

    Initial Commit

---
+++ forecastFit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  forecastFit
+//
+//  Created by Rageeb Mahtab on 10/13/18.
+//  Copyright © 2018 Rageeb Mahtab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
