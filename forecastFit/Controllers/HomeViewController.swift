c207bf630d9cde034c0187873127e1f4dca06be1
Rageeb Mahtab <>
2018-10-13 07:05:42 -0400

    New Changes From Stash

--- forecastFit/Controllers/HomeViewController.swift
+++ forecastFit/Controllers/HomeViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var highTemperatureLabel: UILabel!
+    
+    @IBOutlet weak var lowTemperatureLabel: UILabel!
+    
+    @IBOutlet var weatherDescriptionLabel: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let url = URL(string: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ny%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error{
+                print(error.localizedDescription)
+            } else if let data = data{
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(dataDictionary)
+                let query = dataDictionary["query"] as! [String: Any]
+                print(query)
+            }
         // Do any additional setup after loading the view.
-    }
+        }
     
 
     /*
@@ -27,4 +44,5 @@ class HomeViewController: UIViewController {
     }
     */
 
+    }
 }
